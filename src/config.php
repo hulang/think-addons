dafaee1e06d80138455b639969ac38fc7409b648
快乐浪子哥 <hulangfy@163.com>
2024-01-12 10:14:07 +0800

    update

--- src/config.php
+++ src/config.php
@@ -3,7 +3,8 @@
 declare(strict_types=1);
 
 return [
-    'auth_on'           => 1, // 权限开关
-    'auth_type'         => 1, // 认证方式，1为实时认证；2为登录认证。
-    'auth_user'         => 'member', // 用户信息不带前缀表名
+    'autoload' => true,
+    'hooks'    => [],
+    'route'    => [],
+    'service'  => [],
 ];

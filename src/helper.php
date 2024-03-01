f8c5ccc8cc4bad089160c6b63a1e1f4c67ac05db
快乐浪子哥 <hulangfy@163.com>
2024-03-01 17:28:51 +0800

    Update

--- src/helper.php
+++ src/helper.php
@@ -101,7 +101,6 @@ if (!function_exists('get_addons_instance')) {
         $class = get_addons_class($name);
         if (class_exists($class)) {
             $_addons[$name] = new $class(app());
-
             return $_addons[$name];
         } else {
             return null;
